748f97f9409f5d8c70bafa0221f3056c76d72908
Serghei Iakovlev <sadhooklay@gmail.com>
2019-12-11 00:57:43 +0200

    Initial ConfigMap skeleton

--- include/ytc/configmap.hpp
+++ include/ytc/configmap.hpp
@@ -1,6 +1,17 @@
 #ifndef YTC_CONFIGMAP_HPP_
 #define YTC_CONFIGMAP_HPP_
 
+#include <yaml-cpp/yaml.h>
+#include <memory>
+
 class ConfigMap;
+using ConfigMapPtr = std::shared_ptr<ConfigMap>;
+
+namespace YAML {
+template<> struct convert<ConfigMapPtr> {
+  static Node encode(const ConfigMapPtr &rhs);
+  static bool decode(const Node &node, ConfigMapPtr &c);
+};
+}
 
 #endif  // YTC_CONFIGMAP_HPP_
--- src/configmap/configmap.cpp
+++ src/configmap/configmap.cpp
@@ -1,1 +1,12 @@
 #include "ytc/configmap.hpp"
+
+YAML::Node YAML::convert<ConfigMapPtr>::encode(const ConfigMapPtr &rhs) {
+  Node node;
+  // TODO(serghei): Not implemented
+  return node;
+}
+
+bool YAML::convert<ConfigMapPtr>::decode(const YAML::Node &node, ConfigMapPtr &rhs) {
+  // TODO(serghei): Not implemented
+  return true;
+}
