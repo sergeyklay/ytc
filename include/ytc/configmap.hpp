f702e4a82001044fe0eca2a07903ceba0788850d
Serghei Iakovlev <sadhooklay@gmail.com>
2019-12-11 02:07:26 +0200

    Define base structure

--- include/ytc/configmap.hpp
+++ include/ytc/configmap.hpp
@@ -1,17 +1,33 @@
 #ifndef YTC_CONFIGMAP_HPP_
 #define YTC_CONFIGMAP_HPP_
 
-#include <yaml-cpp/yaml.h>
 #include <memory>
+#include <string>
+#include <yaml-cpp/yaml.h>
 
-class ConfigMap;
+#include "ytc/metadata.hpp"
+
+class ConfigMap {
+public:
+  ConfigMap();
+
+  bool operator==(const ConfigMap &rhs) const;
+
+private:
+  std::string version_;
+  std::string kind_;
+  Metadata meta_;
+  std::map<std::string, std::string> data_;
+  std::string file_;
+};
+
 using ConfigMapPtr = std::shared_ptr<ConfigMap>;
 
 namespace YAML {
-template<> struct convert<ConfigMapPtr> {
+template <> struct convert<ConfigMapPtr> {
   static Node encode(const ConfigMapPtr &rhs);
   static bool decode(const Node &node, ConfigMapPtr &c);
 };
-}
+} // namespace YAML
 
-#endif  // YTC_CONFIGMAP_HPP_
+#endif // YTC_CONFIGMAP_HPP_
--- src/configmap/configmap.cpp
+++ src/configmap/configmap.cpp
@@ -1,4 +1,6 @@
 #include "ytc/configmap.hpp"
+#include <tuple>
+#include <utility>
 
 YAML::Node YAML::convert<ConfigMapPtr>::encode(const ConfigMapPtr &rhs) {
   Node node;
@@ -6,7 +8,19 @@ YAML::Node YAML::convert<ConfigMapPtr>::encode(const ConfigMapPtr &rhs) {
   return node;
 }
 
-bool YAML::convert<ConfigMapPtr>::decode(const YAML::Node &node, ConfigMapPtr &rhs) {
+bool YAML::convert<ConfigMapPtr>::decode(const YAML::Node &node,
+                                         ConfigMapPtr &rhs) {
   // TODO(serghei): Not implemented
   return true;
+}
+
+ConfigMap::ConfigMap()
+    : version_{"v1"}, kind_{"ConfigMap"}, meta_{}, data_{}, file_{""} {}
+
+bool ConfigMap::operator==(const ConfigMap &rhs) const {
+  auto lhs_key = std::tie(version_, kind_, meta_, data_, file_);
+  auto rhs_key =
+      std::tie(rhs.version_, rhs.kind_, rhs.meta_, rhs.data_, rhs.file_);
+
+  return lhs_key == rhs_key;
 }
