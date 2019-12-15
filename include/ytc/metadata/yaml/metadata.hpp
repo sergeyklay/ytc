90e660269e67c69caf66e426c56d9050d9cd85aa
Serghei Iakovlev <sadhooklay@gmail.com>
2019-12-15 19:17:19 +0200

    Add docs

--- include/ytc/metadata/yaml/metadata.hpp
+++ include/ytc/metadata/yaml/metadata.hpp
@@ -6,7 +6,21 @@
 
 namespace YAML {
 template <> struct convert<MetadataPtr> {
+  /**
+   * @brief Encode Metadata object to a Yaml Node.
+   *
+   * @param mptr The smart pointer to a Metadata instance
+   * @return Yaml Node
+   */
   static Node encode(const MetadataPtr &mptr);
+
+  /**
+   * @brief Decode Yaml Node to a Metadata object.
+   *
+   * @param node Yaml Node
+   * @param mptr The smart pointer to a Metadata instance
+   * @return true on success, false otherwise
+   */
   static bool decode(const Node &node, MetadataPtr &mptr);
 };
 } // namespace YAML
