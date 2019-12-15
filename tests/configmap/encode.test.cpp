579e1a623eebbf040e1b7819c567e9144799e640
Serghei Iakovlev <sadhooklay@gmail.com>
2019-12-15 19:17:05 +0200

    Cleaned up tests

--- tests/configmap/encode.test.cpp
+++ tests/configmap/encode.test.cpp
@@ -17,18 +17,26 @@ TEST(ConfigMapTest, EncodeClass) {
       "property.1=value-1\nproperty.2=value-2\nproperty.3=value-3");
 
   ConfigMapPtr cptr = std::make_shared<ConfigMap>(configmap);
-  YAML::Node node = YAML::convert<ConfigMapPtr>::encode(cptr);
+  YAML::Node yaml = YAML::convert<ConfigMapPtr>::encode(cptr);
 
-  EXPECT_TRUE(node.IsMap());
+  EXPECT_TRUE(yaml.IsMap());
 
-  EXPECT_EQ("v1", node["apiVersion"].as<std::string>());
-  EXPECT_EQ("ConfigMap", node["kind"].as<std::string>());
-  EXPECT_EQ("2019-12-31T23:59:59Z",
-            node["metadata"]["creationTimestamp"].as<std::string>());
-  EXPECT_EQ("ho-ho-ho", node["metadata"]["name"].as<std::string>());
-  EXPECT_EQ("old-year", node["metadata"]["namespace"].as<std::string>());
-  EXPECT_EQ("hello", node["data"]["example.property.1"].as<std::string>());
-  EXPECT_EQ("world", node["data"]["example.property.2"].as<std::string>());
+  EXPECT_EQ("v1", yaml["apiVersion"].as<std::string>());
+  EXPECT_EQ("ConfigMap", yaml["kind"].as<std::string>());
+
+  EXPECT_TRUE(yaml["metadata"].IsMap());
+  auto md = yaml["metadata"];
+
+  EXPECT_EQ("2019-12-31T23:59:59Z", md["creationTimestamp"].as<std::string>());
+  EXPECT_EQ("ho-ho-ho", md["name"].as<std::string>());
+  EXPECT_EQ("old-year", md["namespace"].as<std::string>());
+
+  EXPECT_TRUE(yaml["data"].IsMap());
+  auto data = yaml["data"];
+
+  EXPECT_EQ("hello", data["example.property.1"].as<std::string>());
+  EXPECT_EQ("world", data["example.property.2"].as<std::string>());
+
   EXPECT_EQ("property.1=value-1\nproperty.2=value-2\nproperty.3=value-3",
-            node["example.property.file"].as<std::string>());
+            yaml["example.property.file"].as<std::string>());
 }
--- tests/metadata/decode.test.cpp
+++ tests/metadata/decode.test.cpp
@@ -4,19 +4,23 @@
 #include <ytc/metadata.hpp>
 
 TEST(MetadataTest, DecodeString) {
-  YAML::Node node =
-      YAML::Load("metadata: {creationTimestamp: 2016-02-18T19:14:38Z, "
-                 "name: example-config, namespace: default}");
+  YAML::Node yaml = YAML::Load(R"(
+{
+  name: example-config,
+  namespace: default,
+  creationTimestamp: 2016-02-18T19:14:38Z
+}
+)");
 
   Metadata expected("2016-02-18T19:14:38Z", "example-config", "default");
   MetadataPtr actual = std::make_shared<Metadata>();
 
-  EXPECT_TRUE(YAML::convert<MetadataPtr>::decode(node["metadata"], actual));
+  EXPECT_TRUE(YAML::convert<MetadataPtr>::decode(yaml, actual));
   EXPECT_EQ(*actual, expected);
 }
 
 TEST(MetadataTest, DecodeInvalid) {
-  YAML::Node node = YAML::Load("foo: bar");
+  YAML::Node yaml = YAML::Load("foo: bar");
   MetadataPtr actual = std::make_shared<Metadata>();
-  EXPECT_FALSE(YAML::convert<MetadataPtr>::decode(node["foo"], actual));
+  EXPECT_FALSE(YAML::convert<MetadataPtr>::decode(yaml["foo"], actual));
 }
